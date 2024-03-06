d0792d43f0af839fa519840afbf42fc813bfc0fd
youngday <young.day@aliyun.com>
2024-03-07 01:17:05 +0800

    c++20

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,5 @@
 set_project("xmake-example")
-set_languages("c++17")
+set_languages("c++20")
 add_rules("mode.debug", "mode.release")
 -- add_requires("onnxruntime 1.17.0",{configs = {gpu = true}})
 add_requires("onnxruntime 1.17.0")
